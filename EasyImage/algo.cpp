1fd7a04a69f3776c73a4483249fbec638cd1fc14
zyyin <zhiyeyin@gmail.com>
2013-09-08 22:23:32 +0800

    HDR

--- EasyImage/algo.cpp
+++ EasyImage/algo.cpp
@@ -139,38 +139,105 @@ void  Sketch(Mat& img, Mat& dst)
 	}
 	cvtColor(tmp, dst, CV_GRAY2BGR);
 }
+bool Gamma(Mat& img, float gamma, float gain)
+{
+    CFunctionLog fl(__FUNCTION__);
+    if (gamma <= 0.0f) return false;
+    double dinvgamma = 1/gamma*gain;
+    double dMax = pow(255.0, dinvgamma) / 255.0;
+    int dim(256);  
+    Mat lut(1,&dim,CV_8U);  
+    for(int i = 0; i < 256; i++)  
+    {
+        lut.at<uchar>(i) = saturate_cast<uchar>( pow((double)i, dinvgamma) / dMax);
+    }
+    LUT(img, lut, img);
+    return true;
+}
 
+void Curve(Mat& img)
+{
+    int w = img.cols;
+    int h = img.rows;
+    int size = GetYUVSize(w, h, YUV_444);
+    uchar *pBuffer = new uchar[size];
+
+    static uchar curveTable[256] = 
+    {
+        0,3,6,9,12,14,17,20,23,26,29,32,34,37,39,42,44,47,
+        49,51,53,55,57,59,60,62,63,64,66,67,68,69,70,71,72,
+        73,74,75,75,76,77,78,79,80,80,81,82,83,83,84,85,85,
+        86,87,87,88,89,89,90,90,91,91,92,93,93,94,94,95,95,
+        96,96,97,97,98,98,99,99,100,100,101,101,102,102,103,
+        103,104,104,105,106,106,107,107,108,108,109,110,110,111,
+        112,112,113,114,114,115,116,116,117,118,118,119,120,120,
+        121,122,122,123,124,124,125,126,126,127,128,128,128,128,
+        128,128,128,129,130,131,132,133,134,135,136,137,138,139,
+        140,141,142,143,144,145,146,147,148,149,150,151,152,153,
+        154,155,156,157,158,159,160,161,162,163,164,165,166,167,
+        168,169,170,171,172,173,174,175,176,177,178,179,180,181,
+        182,183,184,185,186,187,188,189,190,191,192,193,194,195,
+        196,197,198,199,200,201,202,203,204,205,206,207,208,209,
+        210,211,212,213,214,215,216,217,218,219,220,221,222,223,
+        224,225,226,227,228,229,230,231,232,233,234,235,236,237,
+        238,239,240,241,242,243,244,245,246,247,248,249,250,251,
+        252,253,254,255,
+    };
+
+    ImageBGR2YUV(img, pBuffer, YUV_444);
+    int temp;
+    for(int i = 0; i < w*h; i++)
+    {
+        temp = pBuffer[i];
+        pBuffer[i] = curveTable[temp];
+
+    }
+
+    ImageYUV2BGR(img, pBuffer, YUV_444);
+    delete pBuffer;
+}
 void  Dehaze(Mat& src, Mat& dst)
 {
-	CFunctionLog fl(__FUNCTION__);
-	const double DEHAZE_RATIO = 0.7;
-	int w = src.cols;
-	int h = src.rows;
+    CFunctionLog fl(__FUNCTION__);
 
-	uchar* pData  = src.data;
-	uchar* pDst  = dst.data;
+    Scalar avg = mean(src);
+    double y=0.3*avg.val[2]+0.59*avg.val[1]+0.11*avg.val[0];
+    y = min(150., max(20., y));
+    LOGE(L"y=%f\n", y);
+    const uchar LOW_THREASHOLD = 128;
+    const double DEHAZE_RATIO = (y-20)*0.005;
+    const double nGamma = 1.857-0.00615*y;
+    int w = src.cols;
+    int h = src.rows;
 
-	double minArray[3];
-	double minVal;
+    uchar* pData  = src.data;
+    uchar* pDst  = dst.data;
 
-	for(int i = 0; i < w*h*3; i+=3)
-	{	
-		for(int k = 0; k < 3; k++)
-		{
-			minArray[k] = *pData++;
-		}
-		minVal = min(minArray[0],min(minArray[1], minArray[2] ));
+    double minArray[3];
+    double minVal;
 
-		double dval = minVal;
-		dval = 255 - DEHAZE_RATIO*dval;
-		dval/=255.0;
-		if(dval < 0.1) dval = 0.1;
-		for(int k = 0; k < 3; k++)
-		{
-			double ix = *pDst;
-			*pDst++ = saturate_cast<uchar>((ix - 255)/dval + 255);
-		}
-	}
+    Gamma(dst,  nGamma,  1.0);
+    if(DEHAZE_RATIO > 0.0)
+    {
+
+    for(int i = 0; i < w*h*3; i+=3)
+        {
+            for(int k = 0; k < 3; k++)
+            {
+                minArray[k] = *pData++;
+            }
+            minVal = min(minArray[0],min(minArray[1], minArray[2] ));
+            double dval = minVal;
+            dval = 255 - DEHAZE_RATIO*dval;
+            dval/=255.0;
+            if(dval < 0.1) dval = 0.1;
+            for(int k = 0; k < 3; k++)
+            {
+                double ix = *pDst;
+                *pDst++ = saturate_cast<uchar>((ix - 255)/dval + 255);
+            }
+        }
+    }
 }
 
 void  SoftGlow(Mat& img, Mat& dst)
